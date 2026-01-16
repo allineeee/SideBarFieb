ef3d1296277f0923c43d8c55052417bc5a1198fe
Alline <alline.leal@fieb.edu.br>
2026-01-16 15:45:55 -0300

    Add files via upload

---
+++ SideBarFieb/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const button = document.getElementById("toggleSidebar");
+    const root = document.documentElement;
+
+    if (!sidebar || !button) return;
+
+    // Estado inicial do botão
+    if (root.classList.contains("sidebar-collapsed")) {
+        button.innerHTML = "➡";
+    } else {
+        button.innerHTML = "⬅ Recolher";
+    }
+
+    button.addEventListener("click", function () {
+        root.classList.toggle("sidebar-collapsed");
+
+        const collapsed = root.classList.contains("sidebar-collapsed");
+        localStorage.setItem("sidebarCollapsed", collapsed);
+
+        button.innerHTML = collapsed ? "➡" : "⬅ Recolher";
+    });
+});
